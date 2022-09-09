f07fefd24b43ef6079243863753071a5977a81cd
ruslanzverkov <createmed@mail.ru>
2022-09-09 20:53:41 +0300

    1

---
+++ src/Truck.java
@@ -0,0 +1,20 @@
+public class Truck extends Car {
+
+
+
+        public String modelName;
+        public int wheelsCount;
+
+        public void updateTyre() {
+            System.out.println("Меняем покрышку");
+        }
+
+        public void checkEngine() {
+            System.out.println("Проверяем двигатель");
+        }
+
+        public void checkTrailer() {
+            System.out.println("Проверяем прицеп");
+        }
+
+}
